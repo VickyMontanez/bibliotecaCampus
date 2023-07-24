d5aed78d651ff4f92b3b79e7dd90813338955ed7
VickyMontanezCampus <vmontanez707@gmail.com>
2023-07-24 09:07:28 -0500

    feat: :sparkles: Ninth Query Finished
    Ninth Query Finished
    
    Obtain the borrowed books and their return date

--- routers/prestamo.js
+++ routers/prestamo.js
@@ -21,4 +21,19 @@ appPrestamo.get("/", (req,res)=>{
     });
 });
 
+//9.
+appPrestamo.get("/prestados", (req,res)=>{
+    connection.query(`
+        SELECT  l.titulo AS libro, p.estado AS prestamo, p.fecha_devolucion AS fecha_devolucion
+        FROM libro l
+        INNER JOIN prestamo p
+        ON l.id_libro = p.id_prestamo WHERE p.estado = "Prestado";`, (err, result) => {
+        if (err) {
+            console.error("¡ERROR! I can't show you the books currently checked out :(", err);
+            return res.status(500).json({ mensaje: "¡ERROR! I can't show you the books currently checked out :(" });
+        };
+        res.end(JSON.stringify(result))
+    });
+});
+
 export default appPrestamo;
