d5ec661e6949534ca079906a9f49137c6e9caf59
VickyMontanezCampus <vmontanez707@gmail.com>
2023-07-26 08:20:05 -0500

    feat: :sparkles: Fifteenth Query Finished
    Fifteenth Query Finished : Get the books checked out to a specific user

--- routers/prestamo.js
+++ routers/prestamo.js
@@ -36,4 +36,22 @@ appPrestamo.get("/prestados", (req,res)=>{
     });
 });
 
+//15. Get the books checked out to a specific user (example: Maria Gomez).
+appPrestamo.get("/:id", (req,res)=>{
+    let usuario = req.params.id
+    connection.query(`
+        SELECT l.titulo, u.nombre, u.apellido, p.fecha_prestamo, p.fecha_devolucion
+        FROM libro l
+        INNER JOIN autor a ON l.id_autor = a.id_autor
+        INNER JOIN prestamo p ON l.id_libro = p.id_libro
+        INNER JOIN usuario u ON u.id_usuario = p.id_usuario WHERE u.id_usuario = ?`,
+        [usuario],(err, result) => {
+        if (err) {
+            console.error("¡ERROR! I can't you the user who owes books :(", err);
+            return res.status(500).json({ mensaje: "¡ERROR! I can't you the user who owes books :(" });
+        };
+        res.end(JSON.stringify(result))
+    });
+});
+
 export default appPrestamo;
